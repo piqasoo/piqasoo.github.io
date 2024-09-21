c20a687bf4f9640e35a5101da139e0821df341a9
piqasoo <piqasoo@gmail.com>
2024-09-21 18:57:11 +0400

    test

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/redberry_bootcamp/" : "/",
+    publicPath: process.env.NODE_ENV === "github-pages" ? "/redberry_bootcamp/" : "/",
   };
